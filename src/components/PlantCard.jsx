b82cd11c64dac2d06664e63e8877cf9ac5c8d599
Enoch Mwenda <enochmungiria@gmail.com>
2026-08-14 15:08:45 +0300

    plantsy lab

--- src/components/PlantCard.jsx
+++ src/components/PlantCard.jsx
@@ -1,15 +1,27 @@
 import React from "react";
 
-function PlantCard() {
+function PlantCard({ plant, onUpdatePlant }) {
+  function handleToggleStock() {
+    fetch(`http://localhost:6001/plants/${plant.id}`, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ isInStock: !plant.isInStock }),
+    })
+      .then((res) => res.json())
+      .then((updatedPlant) => onUpdatePlant(updatedPlant));
+  }
+
   return (
     <li className="card" data-testid="plant-item">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      <img src={plant.image} alt={plant.name} />
+      <h4>{plant.name}</h4>
+      <p>Price: ${plant.price}</p>
+      {plant.isInStock ? (
+        <button className="primary" onClick={handleToggleStock}>
+          In Stock
+        </button>
       ) : (
-        <button>Out of Stock</button>
+        <button onClick={handleToggleStock}>Out of Stock</button>
       )}
     </li>
   );
--- src/components/PlantPage.jsx
+++ src/components/PlantPage.jsx
@@ -1,15 +1,47 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([])
+  const [searchTerm, setSearchTerm]= useState("")
+
+  useEffect(()=>{
+    fetch("http://localhost:6001/plants")
+    .then((res)=> res.json())
+    .then((data)=>setPlants(data));
+  }, []);
+
+  function handleAddPlant(newPlant){
+    fetch("http://localhost:6001/plants", {
+      method:"POST",
+      headers: {"Content-Type": "application/json" },
+      body: JSON.stringify(newPlant),
+    })
+    .then((res)=> res.json())
+    .then((createdPlant)=>SetPlants([...plants, createdPlant]))
+
+  }
+  function handleUpdatePlant(updatedPlant){
+    setPlants(
+      plants.map((plant)=>
+        plant.id === updatedPlant.id ? updatedPlant : plant
+    )
+    )
+  }
+
+  const filteredPlants = plants.filter((plant)=>
+  plant.name.toLowerCase().includes(searchTerm.toLowerCase())
+);
+
   return (
-    <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
-    </main>
+    <div>
+      <NewPlantForm onAddPlant={handleAddPlant} />
+      <Search searchTerm={searchTerm} onSearchChange={setSearchTerm} />
+      <PlantList  plants={filteredPlants} onUpdatePlant={handleUpdatePlant}/>
+    </div>
   );
 }
 
